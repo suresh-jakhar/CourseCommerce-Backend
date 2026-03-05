cd4aa4809b9ea51f0d33ca6bff7733f9ed166ecf
suresh-jakhar <gettingsuresh@gmail.com>
2026-03-05 06:17:47 +0530

    Create DataSchema

---
+++ db/db.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const adminSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    creatorId: mongoose.Schema.Types.ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId: mongoose.Schema.Types.ObjectId,
+    courseId: mongoose.Schema.Types.ObjectId
+});
+
+const userModel = mongoose.model("users", userSchema);
+const adminModel = mongoose.model("admins", adminSchema);
+const courseModel = mongoose.model("courses", courseSchema);
+const purchaseModel = mongoose.model("purchases", purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+};
