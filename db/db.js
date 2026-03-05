ee19e5c88a493ed2af360d95a3739259ba1199cf
suresh-jakhar <gettingsuresh@gmail.com>
2026-03-05 06:29:52 +0530

    setup express server with MongoDB

--- db/db.js
+++ db/db.js
@@ -27,6 +27,7 @@ const courseSchema = new Schema({
     title: String,
     description: String,
     price: Number,
+    imageUrl : String,
     creatorId: mongoose.Schema.Types.ObjectId
 });
 
--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
-const {adminRouter} = require("./routes/admin");
-const {userModel, adminModel, courseModel, purchaseModel} = require("./db");
+const { adminRouter } = require("./routes/admin");
+
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("api/v1/user", userRouter);
-app.use("api/v1/course", courseRouter);
-app.use("api/v1/admin",adminRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/admin", adminRouter);
 
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL);
 
-app.listen(3000);
+    app.listen(3000, () => {
+        console.log("Server running on port 3000");
+    });
+}
+
+main();
