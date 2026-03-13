a9d77b3301d4d2f60767300670ac43ae2559376b
Suresh Jakhar <gettingsuresh@gmail.com>
2026-03-13 16:57:02 +0530

    set up basic routing with placeholder pages
    Signed-off-by: Suresh Jakhar <gettingsuresh@gmail.com>

---
+++ client/src/pages/Courses.jsx
@@ -0,0 +1,7 @@
+export default function Courses() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-white">Courses</h1>
+    </div>
+  )
+}
