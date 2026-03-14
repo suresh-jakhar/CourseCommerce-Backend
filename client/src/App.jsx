948b05d716e7c85537439d3b0c2443248782320c
Suresh Jakhar <gettingsuresh@gmail.com>
2026-03-14 13:27:50 +0530

    protected-routes-guard
    Signed-off-by: Suresh Jakhar <gettingsuresh@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,21 @@ import Announcements from './pages/Announcements'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import AppShell from './components/AppShell'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <AppShell>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
+        <Route
+          path="/courses"
+          element={
+            <ProtectedRoute>
+              <Courses />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/community" element={<Community />} />
         <Route path="/channels" element={<Channels />} />
         <Route path="/announcements" element={<Announcements />} />
